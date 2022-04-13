17e0260c7a1b817cb7058eb5dae033e9393bc936
jmatheusbueno <jmatheus_05@hotmail.com>
2022-04-12 22:14:14 -0300

    Adicionando requisição para api do Deezer e começando o front dos cards de musica

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -1,12 +1,15 @@
 function search() {
+    let result;
     let search = document.querySelector('[name="search"]').value;
     var http = new XMLHttpRequest();
-    http.open("get", `https://api.deezer.com/search?q=${search}`, true);
-    http.send();
+    http.open("get", `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${search}`, true);
     http.onreadystatechange = function() {
         if (http.readyState === 4 && http.status === 200) {
-            const json = JSON.parse(http.responseText);
-            console.log(json);
+            result = JSON.parse(http.responseText);
+            console.log(result);
         }
     }
+    http.send();
+
+    console.log(json);
 }
