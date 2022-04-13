ec0d6591172c5bb0783ded7a06ad7a0b565cd660
jmatheusbueno <jmatheus_05@hotmail.com>
2022-04-13 20:46:39 -0300

    Iniciando desenvolvimento da autenticação com Deezer e alterações no front

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -10,6 +10,22 @@ function search() {
         }
     }
     http.send();
+}
 
-    console.log(json);
+function login() {
+    console.log(window.location.hostname);
+    let result;
+    var http = new XMLHttpRequest();
+    http.open(
+        "get", 
+        `https://connect.deezer.com/oauth/auth.php?app_id=1&redirect_uri=https://developers.deezer.com/api/oauth&perms=basic_access,email`, 
+        true
+    );
+    http.onreadystatechange = function() {
+        if (http.readyState === 4 && http.status === 200) {
+            result = JSON.parse(http.responseText);
+        }
+    }
+    http.send();
+    console.log(result);
 }
