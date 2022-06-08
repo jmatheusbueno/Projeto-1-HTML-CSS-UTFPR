63b7558bfd6a62ed5f0ea8796d79e602e26033b1
jmatheusbueno <jmatheus_05@hotmail.com>
2022-06-07 22:48:19 -0300

    criando modal de login

---
+++ Client/assets/scripts/login.js
@@ -0,0 +1,7 @@
+function closeModal() {
+    document.querySelector('.login-modal').style.display = 'none';
+}
+
+function openModal() {
+    document.querySelector('.login-modal').style.display = 'block';
+}
