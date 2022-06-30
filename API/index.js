3113018992d8748fe3bd791daf3889fe56f227de
jmatheusbueno <jmatheus_05@hotmail.com>
2022-06-30 20:05:49 -0300

    criado autenticação jwt

--- API/index.js
+++ API/index.js
@@ -1,7 +1,11 @@
 // config inicial
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const bp = require('body-parser')
+const bp = require('body-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 
 const app = express();
 
@@ -21,14 +25,17 @@ app.get('/', (req, res) => {
 });
 
 // entregar uma porta
-const DB_USER = 'joao';
-const DB_PASSWORD = encodeURIComponent('zftXr5wsKSKVmcUu');
+// const DB_USER = '###';
+// const DB_PASSWORD = encodeURIComponent('###');
+
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@projetoweb.7mldl.mongodb.net/projeto_web?retryWrites=true&w=majority`)
     .then(() => {
         console.log('Conectado ao MongoDB');
         app.use(bp.json())
-    app.use(bp.urlencoded({ extended: true }))
+        app.use(bp.urlencoded({ extended: true }))
         app.listen(3000);
     })
     .catch((err) => console.log(err));
--- API/routes/loginRoutes.js
+++ API/routes/loginRoutes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const Login = require('../models/Login');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 // Salvar login
-router.post('/', async (req, res) => {
+router.post('/auth/register', async (req, res) => {
 
     if (!req.body) {
         res.status(422).json({ error: 'Os dados de usuário não foram enviados' });
@@ -10,16 +12,55 @@ router.post('/', async (req, res) => {
 
     const { username, password  } = req.body;
 
-    const login = {
+    const userExists = await Login.findOne({ username });
+    if (userExists) {
+       return res.status(422).json({ msg: 'Esta conta já está em uso' });
+    }
+
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const login = new Login({
         username,
-        password,
-    };
+        password: passwordHash,
+    });
     
     try {
         await Login.create(login);
         res.status(201).json({ message: 'Usuário cadastrado com sucesso' });
     } catch (err) {
         res.status(500).json({ error: err})
+    }
+});
+
+// Login User
+router.post("/auth/login", async (req, res) => {
+    const {username, password} = req.body;
+
+    const user = await Login.findOne({ username });
+    if (!user) {
+       return res.status(422).json({ msg: 'Usuário não encontrado' });
+    }
+
+    const checkPassword = await bcrypt.compare(password, user.password);
+    if (!checkPassword) {
+        return res.status(422).json({ msg: 'Senha inválida' });
+    }
+
+    try {
+        const secret = '123';
+        console.log(secret);
+        const token = jwt.sign(
+            {
+                id: user._id,
+            }, 
+            secret,
+        );
+
+        res.status(200).json({ msg: 'Autenticação realizada com sucesso', token });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: 'Ocorreu um erro no servidor' });
     }
 });
 
@@ -45,5 +86,35 @@ router.get('/get/:username/:password', async (req, res) => {
         res.status(500).json({ error: err });
     }
 });
+
+// Private Route
+router.get('/get/:id', checkToken, async (req, res) => {
+    const id = req.params.id;
+    const user = await Login.findById(id, '-password');
+
+    if (!user) {
+        res.status(500).json({ error: 'Usuário não encontrado' });
+    };
+
+    res.status(200).json({ user });
+
+});
+
+function checkToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ msg: 'Acesso negado' });
+    }
+
+    try {
+        const secret = '123';
+        jwt.verify(token, secret);
+        next();
+    } catch (err) {
+        res.status(400).json({ msg: 'Token invalido' });
+    }
+}
 
 module.exports = router;
